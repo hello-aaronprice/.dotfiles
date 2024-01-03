aa4479ed2c4ca80f5fae9476c25a04736c0d3b79
Aaron Price <hello-aaronprice@outlook.com>
2024-01-03 15:42:17 +0800

    Tmux and cheatsheet

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 # .dotfiles
-There is no place like home
 
+There is no place like home
 
 ### Installing
 
 ```console
 $ git clone git@github.com:hello-aaronprice/.dotfiles.git .dotfiles
 $ cd ~/.dotfiles
-$ make
+$ stow
 ```
 
 This will create symlinks from this repo to your home folder.
+
+### TODO
+
+- get stow working
+- zsh_profile
---
+++ cht.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# chmod +x filename
+
+languages=$(echo "golang c cpp typescript rust python bash powershell" | tr " " "\n") 
+core_utils=$(echo "find xargs sed awk ls grep"| tr " " "\n")
+
+selected=$(echo -e "$languages\n$core_utils" | fzf)
+
+read -p "QUERY: " query
+
+if echo "$languages" | grep -qs $selected; then
+    tmux split-window -p 33 -h bash -c "curl cheat.sh/$selected/$(echo "$query" | tr " " "+") | less" 
+else
+    tmux split-window -p 33 -h bash -c "curl cheat.sh/$selected~$query | less" 
+fi 
