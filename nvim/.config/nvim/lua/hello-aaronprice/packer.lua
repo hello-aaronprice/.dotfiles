84f31a030d89b08ac868424d3716dcb314340f5c
Aaron Price <hello-aaronprice@outlook.com>
2024-03-01 10:59:20 +0800

    Added Kanagawa NeoVim theme

--- nvim/.config/nvim/lua/hello-aaronprice/packer.lua
+++ nvim/.config/nvim/lua/hello-aaronprice/packer.lua
@@ -20,6 +20,7 @@ return require('packer').startup(function(use)
 		  vim.cmd('colorscheme rose-pine')
 	  end
   })
+  use('rebelot/kanagawa.nvim')
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('nvim-treesitter/playground') 
   use('theprimeagen/harpoon') 
