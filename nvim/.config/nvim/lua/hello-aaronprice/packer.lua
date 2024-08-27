ff1440971fcb7416bf55e0c1632fdeffef907195
Aaron Price <hello-aaronprice@outlook.com>
2024-08-27 16:22:15 +0800

    new alacritty font

--- nvim/.config/nvim/lua/hello-aaronprice/packer.lua
+++ nvim/.config/nvim/lua/hello-aaronprice/packer.lua
@@ -14,7 +14,7 @@ return require('packer').startup(function(use)
   }
   use('rebelot/kanagawa.nvim')
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('nvim-treesitter/playground') 
+  use('nvim-treesitter/playground')
   use('theprimeagen/harpoon') 
   use('mbbill/undotree') 
   use('tpope/vim-fugitive')
