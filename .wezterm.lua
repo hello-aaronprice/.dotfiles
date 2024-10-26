a5686ebd0b07a88f91b0ea608c081548403a70ec
Aaron Price <hello-aaronprice@outlook.com>
2024-10-26 22:14:30 +0800

    feat: back to wezterm, gfx issues with alacritty

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,16 +6,19 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
+-- config.color_scheme = 'Afterglow'
+-- config.color_scheme = 'Github Dark (Gogh)'
+config.color_scheme = "Rosé Pine (Gogh)"
+
 config.colors = {
 	foreground = "#d8d8d8",
 	background = "#181818",
 }
 
+-- config.font = wezterm.font("MesloLGSDZ Nerd Font Mono")
 config.font = wezterm.font("0xProto Regular")
-config.font_size = 10
+config.font_size = 9
 config.enable_tab_bar = false
-config.window_decorations = "RESIZE"
+-- config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.9
-
--- and finally, return the configuration to wezterm
 return config
