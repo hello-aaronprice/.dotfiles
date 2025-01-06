e4e41525183ed2b4d07d59fc1528f943cdf277a9
Aaron Price <hello-aaronprice@outlook.com>
2025-01-06 12:03:00 +0800

    update: disabled lualine

--- nvim/.config/nvim/lua/plugins/main.lua
+++ nvim/.config/nvim/lua/plugins/main.lua
@@ -5,4 +5,13 @@ return {
       colorscheme = "catppuccin",
     },
   },
+  {
+    "nvim-lualine/lualine.nvim",
+    enabled = false,
+  },
+  { "echasnovski/mini.statusline", version = "*", opts = {} },
+  {
+    "echasnovski/mini.icons",
+    version = "*",
+  },
 }
